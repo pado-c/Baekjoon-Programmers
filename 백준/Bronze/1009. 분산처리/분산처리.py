a9034744087440c0ce1dcc3d3688da3f94a26a5a
pado <107686005+pado-c@users.noreply.github.com>
2022-08-07 23:47:44 +0900

    [Bronze II] Title: 분산처리, Time: 168 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,24 @@
+import sys
+imput = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    A, B = map(int,input().split())
+    a = A % 10
+    
+    if a == 0:
+        print(10)
+    elif a in [1,5,6]: 
+        print(a)
+    elif a in [4,9]:
+        b = B % 2
+        if b == 0:
+            print(a*a % 10)
+        else:
+            print(a)
+    else:
+        b = B % 4
+        if b == 0:
+            print(a**4 % 10)
+        else:
+            print(a**b %10)
