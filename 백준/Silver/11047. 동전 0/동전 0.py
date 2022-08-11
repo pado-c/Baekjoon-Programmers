2577e4fde257bd481c31355b969890fcd10da2cb
pado <107686005+pado-c@users.noreply.github.com>
2022-08-11 14:50:53 +0900

    [Silver IV] Title: 동전 0, Time: 156 ms, Memory: 37936 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,19 @@
+from calendar import c
+import sys
+input = sys.stdin.readline
+
+n, k = map(int,input().split()) #동전 종류 n, 가치의 합 k
+coins = []
+for _ in range(n):
+  coins.insert(0, int(input())) #입력받음과 동시에 내림차순으로 정렬
+
+cnt = 0 #필요 동전 개수
+for coin in coins:
+  if coin > k: # k보다 coin의 값이 크면
+    continue
+  else: #크거나 같으면
+    num = k//coin
+    k -= coin * num
+    cnt += num
+
+print(cnt)
