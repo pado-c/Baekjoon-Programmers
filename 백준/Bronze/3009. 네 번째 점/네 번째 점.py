ca9dabd7355d34af97af6fe9f8545a4a1267d53b
pado <107686005+pado-c@users.noreply.github.com>
2022-09-06 19:06:45 +0900

    [Bronze III] Title: 네 번째 점, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,25 @@
+x_dict = {}
+y_dict = {}
+res = []
+
+for _ in range(3):
+  x, y = map(int,input().split())
+  if x in x_dict:
+    x_dict[x] += 1
+  else:
+    x_dict[x] = 1
+  
+  if y in y_dict:
+    y_dict[y] += 1
+  else:
+    y_dict[y] = 1
+
+for k, v in x_dict.items():
+  if v == 1:
+    res.append(k)
+
+for k, v in y_dict.items():
+  if v == 1:
+    res.append(k)
+
+print(*res)
